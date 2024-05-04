60834e87b6ec3111dc0e01f9451c761acafcaa4f
Jair1810 <jair18101@hotmail.com>
2024-05-03 20:47:43 -0600

    Header de Home agregado. (Visual)

---
+++ App/Screens/HomeScreen/Header.jsx
@@ -0,0 +1,89 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useUser } from "@clerk/clerk-expo";
+import Colors from "../../Utils/Colors";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export default function Header() {
+  const { user, isLoading } = useUser();
+  return (
+    //Si el usuario esta disponible,cargar la imagen
+    user && (
+      <View style={styles.container}>
+        {/* Profile container */}
+        <View style={styles.profileMainContainer}>
+          <View style={styles.profileContainer}>
+            <Image source={{ uri: user?.imageUrl }} style={styles.userImage} />
+            <View>
+              <Text style={{ color: Colors.WHITE }}>Welcome</Text>
+              <Text style={{ color: Colors.WHITE, fontSize: 20 }}>
+                {user?.fullName}
+              </Text>
+            </View>
+          </View>
+          <FontAwesome name="bookmark-o" size={24} color="white" />
+        </View>
+        {/* Search Bar container */}
+        <View style={styles.searchBarContainer}>
+          <TextInput placeholder="Search" style={styles.textInput} />
+          <TouchableOpacity style={styles.searchbtn}>
+            <FontAwesome name="search" size={24} color={Colors.PRIMARY} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingTop: 40,
+    backgroundColor: Colors.PRIMARY,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+  },
+  profileMainContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  profileContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  userImage: {
+    width: 45,
+    height: 45,
+    borderRadius: 99,
+  },
+  searchBarContainer: {
+    marginTop: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  textInput: {
+    padding: 7,
+    paddingHorizontal: 16,
+    backgroundColor: Colors.WHITE,
+    borderRadius: 8,
+    width: "85%",
+    fontSize: 16,
+  },
+  searchbtn: {
+    backgroundColor: Colors.WHITE,
+    padding: 10,
+    borderRadius: 8,
+  },
+});
