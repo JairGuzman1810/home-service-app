e72d8139b7cc0ece9d1a920575eb1372174f0a6e
Jair1810 <jair18101@hotmail.com>
2024-05-09 14:12:46 -0600

    Validaciones en el boton. Mostrar un activityIndicator cuando se hace la accion del botón

--- App/Screens/BusinessDetailsScreen/BookingModal.jsx
+++ App/Screens/BusinessDetailsScreen/BookingModal.jsx
@@ -1,37 +1,35 @@
+import React, { useEffect, useState } from "react";
 import {
-  StyleSheet,
-  Text,
   View,
+  Text,
   TouchableOpacity,
-  FlatList,
+  StyleSheet,
   KeyboardAvoidingView,
   Platform,
+  ActivityIndicator,
+  ScrollView,
+  TextInput,
 } from "react-native";
-import React, { useEffect, useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import CalendarPicker from "react-native-calendar-picker";
 import Colors from "../../Utils/Colors";
 import Heading from "../../Components/Heading";
-import { ScrollView, TextInput } from "react-native-gesture-handler";
 
 export default function BookingModal({ hideModal }) {
   const [timeList, setTimeList] = useState([]);
   const [selectedTime, setSelectedTime] = useState("");
   const [selectedDate, setSelectedDate] = useState("");
   const [note, setNote] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const getTime = () => {
     const timeList = [];
-
-    // Loop over each half-hour interval from 8 AM to 7:30 PM
     for (let hour = 8; hour <= 19; hour++) {
-      const period = hour < 12 ? "AM" : "PM"; // Determine AM/PM period
-      const displayHour = hour <= 12 ? hour : hour - 12; // Adjust hour for PM times
-
+      const period = hour < 12 ? "AM" : "PM";
+      const displayHour = hour <= 12 ? hour : hour - 12;
       timeList.push({ time: `${displayHour}:00 ${period}` });
       timeList.push({ time: `${displayHour}:30 ${period}` });
     }
-
     setTimeList(timeList);
   };
 
@@ -45,71 +43,87 @@ export default function BookingModal({ hideModal }) {
       style={styles.container}
     >
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <TouchableOpacity style={styles.back} onPress={() => hideModal()}>
-            <Ionicons name="arrow-back" size={30} color="black" />
-            <Text style={styles.categoryTitle}>Booking</Text>
-          </TouchableOpacity>
-          <Heading text={"Select Date"} />
-          {/* Calendario */}
-          <View style={styles.calendar}>
-            <CalendarPicker
-              onDateChange={setSelectedDate}
-              width={340}
-              minDate={Date.now()}
-              todayBackgroundColor={Colors.BLACK}
-              todayTextStyle={{ color: Colors.WHITE }}
-              selectedDayColor={Colors.PRIMARY}
-              selectedDayTextColor={Colors.WHITE}
-              textStyle={{ fontFamily: "Montserrat-Medium" }}
-            />
-          </View>
-          {/* Hora */}
-          <View style={{ marginTop: 20 }}>
-            <Heading text={"Select Time Slot"} />
-
-            <FlatList
-              horizontal
-              data={timeList}
-              showsHorizontalScrollIndicator={false}
-              renderItem={({ item, index }) => (
-                <TouchableOpacity
-                  onPress={() => setSelectedTime(item.time)}
-                  style={{ marginRight: 10 }}
-                >
-                  <Text
-                    style={
-                      selectedTime == item.time
-                        ? styles.selectedTime
-                        : styles.unselectedTime
-                    }
-                  >
-                    {item.time}
-                  </Text>
-                </TouchableOpacity>
-              )}
-            />
-          </View>
-          {/* Notas */}
-          <View style={{ paddingTop: 15 }}>
-            <Heading text={"Any Suggestion Note"} />
-            <TextInput
-              placeholder="Note"
-              numberOfLines={4}
-              multiline
-              onChange={(text) => setNote(text)}
-              style={styles.noteText}
-            />
-          </View>
-          {/* Boton */}
-          <TouchableOpacity style={{ marginTop: 15 }}>
-            <Text style={styles.confirmbtn}>Confirm & Book</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.back} onPress={hideModal}>
+          <Ionicons name="arrow-back" size={30} color="black" />
+          <Text style={styles.categoryTitle}>Booking</Text>
+        </TouchableOpacity>
+        <Heading text="Select Date" />
+        <View style={styles.calendar}>
+          <CalendarPicker
+            onDateChange={setSelectedDate}
+            width={340}
+            minDate={new Date()}
+            todayBackgroundColor={Colors.BLACK}
+            todayTextStyle={{ color: Colors.WHITE }}
+            selectedDayColor={Colors.PRIMARY}
+            selectedDayTextColor={Colors.WHITE}
+            textStyle={{ fontFamily: "Montserrat-Medium" }}
+          />
         </View>
+        <View style={{ marginTop: 20 }}>
+          <Heading text="Select Time Slot" />
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={{ flexDirection: "row" }}
+          >
+            {timeList.map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                onPress={() => setSelectedTime(item.time)}
+                style={{ marginRight: 10 }}
+              >
+                <Text
+                  style={
+                    selectedTime === item.time
+                      ? styles.selectedTime
+                      : styles.unselectedTime
+                  }
+                >
+                  {item.time}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+        <View style={{ paddingTop: 15 }}>
+          <Heading text="Any Suggestion Note" />
+          <TextInput
+            placeholder="Note"
+            multiline
+            onChangeText={setNote}
+            style={styles.noteText}
+          />
+        </View>
+        {isLoading ? (
+          <ActivityIndicator
+            size="large"
+            color={Colors.PRIMARY}
+            style={styles.indicator}
+          />
+        ) : (
+          <TouchableOpacity
+            style={{ marginTop: 15 }}
+            onPress={() => setIsLoading(true)}
+            disabled={selectedTime === "" || selectedDate === ""}
+          >
+            <Text
+              style={
+                selectedTime === "" || selectedDate === ""
+                  ? styles.confirmbtnDisable
+                  : styles.confirmbtn
+              }
+            >
+              Confirm & Book
+            </Text>
+          </TouchableOpacity>
+        )}
       </ScrollView>
     </KeyboardAvoidingView>
   );
 }
+
+// Styles remain unchanged
 
 const styles = StyleSheet.create({
   container: {
@@ -176,4 +190,22 @@ const styles = StyleSheet.create({
     elevation: 4,
     marginBottom: 5,
   },
+  confirmbtnDisable: {
+    textAlign: "center",
+    fontFamily: "Montserrat-Medium",
+    fontSize: 17,
+    backgroundColor: Colors.GRAY,
+    color: Colors.WHITE,
+    padding: 13,
+    borderRadius: 99,
+    // Sombra para iOS
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    // Sombra para Android
+    elevation: 4,
+    marginBottom: 5,
+  },
+  indicator: { padding: 15 },
 });
